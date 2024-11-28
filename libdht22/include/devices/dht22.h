69b4b4185ea4a95d8d79a8acc8bcb06999757fd0
Bradley Remedios <bremedios@gmail.com>
2024-11-27 22:35:20 -0800

    devices/dht22.h - fixed formatting

--- libdht22/include/devices/dht22.h
+++ libdht22/include/devices/dht22.h
@@ -7,22 +7,22 @@ namespace devices {
     class dht22 {
     public:
         dht22();
-	~dht22();
+		~dht22();
 
-	bool Create();
-	bool Read(int count=5);
+		bool Create();
+		bool Read(int count=5);
 
-	float getTemperature() const { return m_temperature; }
-	float getHumidity() const { return m_humidity; }
+		float getTemperature() const { return m_temperature; }
+		float getHumidity() const { return m_humidity; }
     private:
-	bool Read_();
+		bool Read_();
 
-	std::chrono::steady_clock::time_point m_nextRead;
-	float                                 m_temperature=0.0f;
-	float                                 m_humidity=0.0f;
-	int                                   m_pin=7;
-	int                                   m_dat[5]={0};
-	int                                   m_readPeriod=2000;
+		std::chrono::steady_clock::time_point m_nextRead;
+		float                                 m_temperature=0.0f;
+		float                                 m_humidity=0.0f;
+		int                                   m_pin=7;
+		int                                   m_dat[5]={0};
+		int                                   m_readPeriod=2000;
 
     }; // dht22
 }; // devices
